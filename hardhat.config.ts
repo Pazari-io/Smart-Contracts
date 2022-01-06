1504a0c9aed73f19f3789cc20650ec2d7be7c575
REGO350 <riokanehiro228pa@gmail.com>
2022-01-06 22:28:03 +0900

    added optimizer option

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -70,7 +70,7 @@ const config: HardhatUserConfig = {
         version: "0.8.9",
         settings: {
           optimizer: {
-            enabled: true,
+            enabled: process.env.OPTIMIZER === "true",
             runs: 800,
           },
         },
