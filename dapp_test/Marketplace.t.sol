faeaadca4e3050f7ac01c2655053134936705ee1
REGO350 <riokanehiro228pa@gmail.com>
2022-01-11 01:43:32 +0900

    marketplace very simple test

--- dapp_test/Marketplace.t.sol
+++ dapp_test/Marketplace.t.sol
@@ -1,56 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-// import "./setup/MarketplaceSetup.sol";
+import "./setup/MarketplaceSetup.sol";
 
 //solhint-disable func-name-mixedcase
 //solhint-disable state-visibility
+//solhint-disable no-unused-vars
 
-// contract MarketplaceTest is MarketplaceSetup {
-//   uint8 numDevs = 5;
-//   uint16 numUsers = 100;
-//   uint16 minTax = 100;
-//   uint16 maxTax = 10000;
-//   uint8 erc1155Amount = 10;
-//   uint8 erc20Amount = 20;
+contract CreateMarketItemTest is MarketplaceSetup {
+  function test_oneType() public {
+    //Mint erc20 and erc1155
+    erc1155Mint(0, address(this), 0, 100);
+    erc20Mint(0, address(this), 100e18);
 
-//   function setUp() public {
-//     super.setUp(numDevs, numUsers);
-//     marketplaceDeploy(minTax, maxTax);
-//     erc1155Deploy(erc1155Amount);
-//     erc20Deploy(erc20Amount);
-//   }
+    //Give operator approval
+    erc1155s[0].setApprovalForAll(address(mp), true);
 
-//   function test_Basic_CreateMarketItem() public {
-//     //Mint erc20 and erc1155
-//     erc1155Mint(0, address(this), 0, 100);
-//     erc20Mint(0, address(this), 100e18);
+    //Create simple payment route
+    (bytes32 routeID, ) = createSimpleRoute();
 
-//     //Give operator approval
-//     erc1155s[0].setApprovalForAll(address(marketplace), true);
+    //Create simple market item
+    uint256 itemID = mp.createMarketItem(
+      address(erc1155s[0]),
+      address(this),
+      0,
+      50,
+      1e18,
+      address(erc20s[0]),
+      true,
+      true,
+      routeID,
+      5,
+      true
+    );
 
-//     //Create simple payment route
-//     address[] memory recipients = new address[](1);
-//     recipients[0] = address(this);
-//     uint16[] memory commissions = new uint16[](1);
-//     commissions[0] = 10000;
-
-//     bytes32 routeID = marketplace.openPaymentRoute(recipients, commissions, 100);
-
-//     //Create simple market item
-//     uint256 itemID = marketplace.createMarketItem(
-//       address(erc1155s[0]),
-//       address(this),
-//       0,
-//       50,
-//       1e18,
-//       address(erc20s[0]),
-//       true,
-//       true,
-//       routeID,
-//       5,
-//       true
-//     );
-
-//     assertEq(itemID, 1);
-//   }
-// }
+    assertEq(itemID, 1);
+  }
+}
--- dapp_test/setup/MarketplaceSetup.sol
+++ dapp_test/setup/MarketplaceSetup.sol
@@ -2,73 +2,178 @@
 pragma solidity ^0.8.0;
 
 //solhint-disable state-visibility
+//solhint-disable reason-string
 
 /* Import lib */
-// import "../utils/Hevm.sol";
-// import "../utils/MockContract.sol";
-// import "../utils/DSTestExtended.sol";
+import "../utils/Hevm.sol";
+import "../utils/MockContract.sol";
+import "../utils/DSTestExtended.sol";
 
 /* Import setups */
-// import {UsersSetup} from "./UsersSetup.sol";
+import "./UsersSetup.sol";
 
 /* Import contracts */
-// import {Marketplace} from "contracts/Marketplace/Marketplace.sol";
-// import {ERC1155PresetMinterPauser} from "contracts/Dependencies/ERC1155PresetMinterPauser.sol";
-// import {ERC20PresetMinterPauser} from "contracts/Dependencies/ERC20PresetMinterPauser.sol";
+import {PaymentRouter} from "contracts/PaymentRouter/PaymentRouter.sol";
+import {Marketplace} from "contracts/Marketplace/Marketplace.sol";
+import {ERC20} from "contracts/Dependencies/ERC20.sol";
+import {ERC20PresetMinterPauser} from "contracts/Dependencies/ERC20PresetMinterPauser.sol";
+import {ERC1155PresetMinterPauser} from "contracts/Dependencies/ERC1155PresetMinterPauser.sol";
+import {ERC1155} from "contracts/Dependencies/ERC1155.sol";
+import {PazariTokenMVP} from "contracts/Tokens/PazariTokenMVP.sol";
 
-// contract MarketplaceSetup is DSTestExtended, UsersSetup {
-//   //Hevm setup
-//   Hevm internal constant HEVM = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+contract MPUser is User {
+  function approveERC20(
+    ERC20 erc20,
+    address to,
+    uint256 amount
+  ) public {
+    erc20.approve(to, amount);
+  }
+}
 
-//   //Contracts
-//   Marketplace marketplace;
-//   ERC1155PresetMinterPauser[] erc1155s;
-//   ERC20PresetMinterPauser[] erc20s;
+contract MarketplaceSetup is DSTestExtended, MPUser {
+  //Hevm setup
+  Hevm internal constant HEVM = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
-//   function setUp(uint8 numDevs, uint16 numUsers) public virtual override {
-//     //Set timestamp to 0
-//     HEVM.warp(0);
+  //Contracts
+  PaymentRouter pr;
+  Marketplace mp;
+  ERC20[] erc20s;
+  ERC1155[] erc1155s;
 
-//     //Setup users
-//     UsersSetup.setUp(numDevs, numUsers);
-//   }
+  //Users
+  MPUser[] devs;
+  MPUser[] users;
+  NFTHolder treasury;
 
-//   function marketplaceDeploy(address paymentRouter) public virtual {
-//     marketplace = new Marketplace(paymentRouter);
-//   }
+  //Settings
+  uint8 numDevs = 5;
+  uint8 numUsers = 10;
+  uint8 erc20Amount = 5;
+  uint8 erc1155Amount = 5;
+  uint16 minTax = 50;
+  uint16 maxTax = 5000;
 
-//   function erc1155Deploy(uint8 amount) public virtual {
-//     for (uint256 i = 0; i < amount; i++) {
-//       erc1155s.push(new ERC1155PresetMinterPauser());
-//     }
-//   }
+  function setUp() public virtual {
+    //Set timestamp to 0
+    HEVM.warp(0);
+    HEVM.roll(0);
 
-//   function erc20Deploy(uint8 amount) public virtual {
-//     for (uint256 i = 0; i < amount; i++) {
-//       erc20s.push(new ERC20PresetMinterPauser("abc", "ABC"));
-//     }
-//   }
+    //Setup users
+    for (uint256 i = 0; i < numDevs; i++) {
+      devs.push(new MPUser());
+    }
+    for (uint256 i = 0; i < numUsers; i++) {
+      users.push(new MPUser());
+    }
+    treasury = new NFTHolder();
 
-//   function erc1155Mint(
-//     uint8 index,
-//     address to,
-//     uint256 id,
-//     uint256 amount
-//   ) public virtual {
-//     string memory uri = "https://api.pazari.io/metadata/";
-//     bytes memory data = "0x";
-//     if (index < erc1155s.length) {
-//       erc1155s[index].mint(to, id, amount, uri, data);
-//     }
-//   }
+    //Deploy contracts
+    paymentRouterDeploy(minTax, maxTax);
+    marketplaceDeploy(address(pr));
+    erc20sDeploy(erc20Amount);
+    erc1155sDeploy(erc1155Amount);
+  }
 
-//   function erc20Mint(
-//     uint8 index,
-//     address to,
-//     uint256 amount
-//   ) public virtual {
-//     if (index < erc20s.length) {
-//       erc20s[index].mint(to, amount);
-//     }
-//   }
-// }
+  function paymentRouterDeploy(uint16 _minTax, uint16 _maxTax) public virtual {
+    address[] memory devsAddr = new address[](devs.length);
+    for (uint256 i = 0; i < devs.length; i++) {
+      devsAddr[i] = address(devs[i]);
+    }
+    pr = new PaymentRouter(address(treasury), devsAddr, _minTax, _maxTax);
+  }
+
+  function marketplaceDeploy(address prAddr) public virtual {
+    mp = new Marketplace(prAddr);
+  }
+
+  function erc1155sDeploy(uint8 amount) public virtual {
+    for (uint256 i = 0; i < amount; i++) {
+      address erc1155Addr = address(new ERC1155PresetMinterPauser());
+      erc1155s.push(ERC1155(erc1155Addr));
+    }
+  }
+
+  function erc20sDeploy(uint8 amount) public virtual {
+    for (uint256 i = 0; i < amount; i++) {
+      address erc20Addr = address(new ERC20PresetMinterPauser("abc", "ABC"));
+      erc20s.push(ERC20(erc20Addr));
+    }
+  }
+
+  function erc1155Mint(
+    uint8 index,
+    address to,
+    uint256 id,
+    uint256 amount
+  ) public virtual {
+    require(index < erc1155s.length, "Invalid array length");
+    string memory uri = "https://api.pazari.io/metadata/";
+    bytes memory data = "0x";
+    if (index < erc1155s.length) {
+      ERC1155PresetMinterPauser erc1155 = ERC1155PresetMinterPauser(address(erc1155s[index]));
+      erc1155.mint(to, id, amount, uri, data);
+    }
+  }
+
+  function erc20Mint(
+    uint8 index,
+    address to,
+    uint256 amount
+  ) public virtual {
+    require(index < erc20s.length, "Invalid array length");
+    ERC20PresetMinterPauser erc20 = ERC20PresetMinterPauser(address(erc20s[index]));
+    erc20.mint(to, amount);
+  }
+
+  function erc20Mint(
+    uint8 index,
+    address[] memory to,
+    uint256 amount
+  ) public virtual {
+    require(index < erc20s.length, "Invalid array length");
+    ERC20PresetMinterPauser erc20 = ERC20PresetMinterPauser(address(erc20s[index]));
+    for (uint256 i = 0; i < to.length; i++) {
+      erc20.mint(to[i], amount);
+    }
+  }
+
+  function createSimpleRoute() public returns (bytes32, address) {
+    (bytes32 routeID, address[] memory recipients, ) = createStandardRoute(1, 100);
+    return (routeID, recipients[0]);
+  }
+
+  function createSimpleRoute(uint16 tax) public returns (bytes32, address) {
+    (bytes32 routeID, address[] memory recipients, ) = createStandardRoute(1, tax);
+    return (routeID, recipients[0]);
+  }
+
+  function createStandardRoute(uint8 num, uint16 tax)
+    public
+    returns (
+      bytes32,
+      address[] memory,
+      uint16[] memory
+    )
+  {
+    require(num > 0, "num > 0");
+    require(tax <= 10000, "tax <= 10000");
+
+    address[] memory recipients = new address[](num);
+    uint16[] memory commissions = new uint16[](num);
+
+    uint16 sum = 0;
+    for (uint8 i = 0; i < num - 1; i++) {
+      recipients[i] = address(new MPUser());
+      commissions[i] = 10000 / num;
+      sum += 10000 / num;
+    }
+    recipients[num - 1] = address(new MPUser());
+    commissions[num - 1] = 10000 - sum;
+
+    //Create payment route
+    bytes32 routeID = pr.openPaymentRoute(recipients, commissions, tax);
+
+    return (routeID, recipients, commissions);
+  }
+}
