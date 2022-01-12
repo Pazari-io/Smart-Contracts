5ec998fcd137e8f168647c84d737559f8215b1a5
PhoenixAngell <86646374+PhoenixAngell@users.noreply.github.com>
2022-01-12 15:01:08 -0800

    Update IPazariMVP.sol
    Patch Notes: Major
     - Added three new events: NewTokenListed, NewUserCreated, and ContractCloned. NewUserCreated and ContractCloned are emitted by createUserProfile(), and contain all the details of the new UserProfile struct as well as data about the new contract that was cloned.
     - Added AccessControlPMVP contract and all functions within
     - Added getMarketItem() and its MarketItem struct
     - Modified recoverNFT() to take in the address of the person recovering their NFT, now requires PazariMVP admin access to call.
     - Changed newUser to createUserProfile and changed the return type to a UserProfile struct instead of a success bool. This will be much more useful for other contracts. This function also emits an event that
    
    Patch Notes: Minor
     - Added a few @return tags to function comments

--- contracts/Helpers/IPazariMVP.sol
+++ contracts/Helpers/IPazariMVP.sol
@@ -2,12 +2,58 @@
 pragma solidity ^0.8.0;
 
 interface IPazariMVP {
+
+  // Fires when a new token is listed for sale
+  event NewTokenListed(
+    address indexed itemOwner,
+    uint256 itemID,
+    uint256 indexed price,
+    address indexed tokenContract,
+    uint256 tokenID,
+    uint256 amount
+  );
+
+  // Fires when a new user joins and lists an item
+  event NewUserCreated(
+    address userAddress, 
+    bytes32 routeID, 
+    address tokenContractAddress, 
+    uint256 itemID, 
+    uint256 timestamp
+  );
+
+  // Fires after a token contract is cloned
+  event ContractCloned(
+    uint contractID, 
+    uint16 indexed contractType,
+    address indexed creatorAddress,
+    address indexed factoryAddress, 
+    address cloneAddress, 
+    uint256 timestamp
+  );
+
+  // Created for everyone who runs newUser()
   struct UserProfile {
     address userAddress;
     address tokenContract;
     bytes32 routeID;
     uint256[] itemIDs;
   }
+
+  // Struct for market items being sold, returned by getMarketItem()
+  struct MarketItem {
+    uint256 itemID;
+    address tokenContract;
+    uint256 tokenID;
+    uint256 amount;
+    uint256 price;
+    address paymentContract;
+    bool isPush;
+    bytes32 routeID;
+    bool routeMutable;
+    bool forSale;
+    uint256 itemLimit;
+  }  
 
   /**
    * @notice Auto-generates a new payment route, clones a token contract, mints a token, and lists
@@ -16,15 +62,17 @@ interface IPazariMVP {
    * @param _URI URL of the JSON public metadata file, usually an IPFS URI
    * @param _amount Amount of tokens to be minted and listed
    * @param _price Price in USD for each token.
+   * @return UserProfile The newly created UserProfile struct
    */
-  function newUser(
+  function createUserProfile(
     string memory _URI,
     uint256 _amount,
     uint256 _price
-  ) external returns (bool);
+  ) external returns (UserProfile memory);
 
   /**
    * @notice Creates a new token and lists it on the Pazari Marketplace
+   * @return tokenID The tokenID and itemID of the new token listed
    *
    * @dev Assumes the seller is using the same PaymentRoute and token contract
    * created in newUser().
@@ -35,16 +83,41 @@ interface IPazariMVP {
     string memory _URI,
     uint256 _amount,
     uint256 _price
-  ) external returns (uint256, uint256);
+  ) external returns (uint256 tokenID, uint256 itemID);
 
   /**
    * @notice This is in case someone mistakenly sends their ERC1155 NFT to this contract address
+   * @dev Only PazariMVP admins can call this function, and is the only function they can call.
    */
   function recoverNFT(
     address _nftContract,
     uint256 _tokenID,
-    uint256 _amount
+    uint256 _amount,
+    address _to
   ) external returns (bool);
 
+  /**
+   * @notice Returns a user's UserProfile struct
+   */
   function getUserProfile(address _userAddress) external view returns (UserProfile memory);
+
+  /**
+   * @notice Returns the MarketItem struct for a single _itemID
+   * @dev If we need to return multiple MarketItems, then use iMarketplace.getMarketItems()
+   * @dev This function is much easier to use as it does not need arrays, but it only returns
+   * one MarketItem struct.
+   */
+  function getMarketItem(uint256 _itemID) external view returns (MarketItem memory);
+
+}
+
+interface AccessControlPMVP {
+
+  function isAdmin(address _userAddress) external view returns (bool);
+
+  function _msgSender() external view returns (address);
+
+  function addAdmin(address _newAddress, string memory _memo) external returns (bool);
+
+  function removeAdmin(address _oldAddress, string memory _memo) external returns (bool);
 }
