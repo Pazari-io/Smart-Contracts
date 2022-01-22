7f8833ff9577c7dd1b4c9d3afb604a3ee96bbfb4
REGO350 <riokanehiro228pa@gmail.com>
2022-01-22 21:04:04 +0900

    import dapp test

--- dapp_test/Marketplace.t.sol
+++ dapp_test/Marketplace.t.sol
@@ -4,40 +4,22 @@ import "./setup/MarketplaceSetup.sol";
 
 //solhint-disable func-name-mixedcase
 //solhint-disable state-visibility
+//solhint-disable no-unused-vars
 
-contract MarketplaceTest is MarketplaceSetup {
-  uint8 numDevs = 5;
-  uint16 numUsers = 100;
-  uint16 minTax = 100;
-  uint16 maxTax = 10000;
-  uint8 erc1155Amount = 10;
-  uint8 erc20Amount = 20;
-
-  function setUp() public {
-    super.setUp(numDevs, numUsers);
-    marketplaceDeploy(minTax, maxTax);
-    erc1155Deploy(erc1155Amount);
-    erc20Deploy(erc20Amount);
-  }
-
-  function test_Basic_CreateMarketItem() public {
+contract CreateMarketItemTest is MarketplaceSetup {
+  function test_oneType() public {
     //Mint erc20 and erc1155
     erc1155Mint(0, address(this), 0, 100);
     erc20Mint(0, address(this), 100e18);
 
     //Give operator approval
-    erc1155s[0].setApprovalForAll(address(marketplace), true);
+    erc1155s[0].setApprovalForAll(address(mp), true);
 
     //Create simple payment route
-    address[] memory recipients = new address[](1);
-    recipients[0] = address(this);
-    uint16[] memory commissions = new uint16[](1);
-    commissions[0] = 10000;
-
-    bytes32 routeID = marketplace.openPaymentRoute(recipients, commissions, 100);
+    (bytes32 routeID, ) = createSimpleRoute();
 
     //Create simple market item
-    uint256 itemID = marketplace.createMarketItem(
+    uint256 itemID = mp.createMarketItem(
       address(erc1155s[0]),
       address(this),
       0,
